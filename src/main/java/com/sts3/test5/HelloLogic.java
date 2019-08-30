1bce9f0fac789be0f5386ba8c15dbdbd666bff30
bigbang-cuk <dndrl0624@naver.com>
2019-08-30 19:40:36 +0900

    push to remote

--- src/main/java/com/sts3/test5/HelloLogic.java
+++ src/main/java/com/sts3/test5/HelloLogic.java
@@ -1,7 +1,12 @@
 package com.sts3.test5;
 
 public class HelloLogic {
-	String msg = "dfsd";
-	String msg2 = "sdfsfsdf";
-	String msg3 = "sdfaagg";
+	String msg = "로컬에서 작업한 내용을 원격으로 push해보자";
+	
+	public void PushToRemote() {
+		System.out.println("앞서 배운대로 작업영역에서 변경된 내용은			");
+		System.out.println("로컬저장소의 commit후보로 unstage상태가 됩니다.");
+		System.out.println("이를 stage영역에 올리고 로컬저장소에 commit하고");
+		System.out.println("원격저장소에 push하면 됩니다.				");
+	}
 }
